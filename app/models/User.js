8bd25e455921dfb1fe0d959095014ddb8903206a
kevchike <kevchike@gmail.com>
2018-12-12 08:11:19 +0100

    Created User model

--- app/models/User.js
+++ app/models/User.js
@@ -1,8 +1,16 @@
 module.exports = {
 	tableName: "users",
 	attributes: {
+		uid: "",
 		name: "",
 		email: "",
-		password: ""
+		phone: "",
+		role_id: "",
+		church_id: "",
+		is_active: "",
+		last_login: "",
+		password: "",
+		created_at: "",
+		updated_at: ""
 	}
 };
